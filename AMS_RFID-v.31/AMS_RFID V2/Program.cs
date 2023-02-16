3e30626d5531ea3737e67e24bcb9f04730a38891
glen0924 <glen24contreras@gmail.com>
2023-02-16 16:05:52 +0800

    Update
    Updates

---
+++ AMS_RFID-v.31/AMS_RFID V2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AMS_RFID_V2
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        ///
+
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new EmployeeAttendance());
+            //Application.Run(new LoginForm());
+            //Application.Run(new SupportingWinform());
+            //Application.Run(new EditEmployeeForm());
+            //Application.Run(new SupportingWinform());
+        }
+    }
+}
